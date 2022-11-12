a298f19bf3241736ae2636b0be26d96ddf9a58b3
stormytuna <stormytuna@outlook.com>
2022-11-12 20:32:01 +0000

    Change stuff to use config

--- Common/CriticalStrikeChance.cs
+++ Common/CriticalStrikeChance.cs
@@ -1,10 +1,11 @@
+using SummoningExpanded.Common.Configs;
 using System.Collections.Generic;
 using Terraria;
 using Terraria.ModLoader;
 
 namespace SummoningExpanded.Common.GlobalProjectiles {
     public class CriticalStrikeChance : GlobalProjectile {
-        public override bool AppliesToEntity(Projectile entity, bool lateInstantiation) => entity.CountsAsClass(DamageClass.Summon);
+        public override bool AppliesToEntity(Projectile entity, bool lateInstantiation) => entity.CountsAsClass(DamageClass.Summon) && ServerConfig.Instance.SummonCrit;
 
         public override void ModifyHitNPC(Projectile projectile, NPC target, ref int damage, ref float knockback, ref bool crit, ref int hitDirection) {
             if (Main.rand.Next(100) < projectile.CritChance) {
@@ -16,7 +17,7 @@ namespace SummoningExpanded.Common.GlobalProjectiles {
 
 namespace SummoningExpanded.Common.GlobalItems {
     public class CriticalStrikeChance : GlobalItem {
-        public override bool AppliesToEntity(Item entity, bool lateInstantiation) => entity.CountsAsClass(DamageClass.Summon);
+        public override bool AppliesToEntity(Item entity, bool lateInstantiation) => entity.CountsAsClass(DamageClass.Summon) && ServerConfig.Instance.SummonCrit;
 
         public override void ModifyTooltips(Item item, List<TooltipLine> tooltips) {
             int index = tooltips.FindIndex(tip => tip.Name == "Damage") + 1;
