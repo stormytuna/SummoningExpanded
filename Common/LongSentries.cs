704ff6f9114d7e47517434dacc1153dffee34677
stormytuna <stormytuna@outlook.com>
2022-11-12 20:32:06 +0000

    Add long sentries

---
+++ Common/LongSentries.cs
@@ -0,0 +1,21 @@
+﻿using SummoningExpanded.Common.Configs;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace SummoningExpanded.Common.GlobalProjectiles {
+    public class LongSentries : GlobalProjectile {
+        public override bool AppliesToEntity(Projectile entity, bool lateInstantiation) => entity.sentry && ServerConfig.Instance.SentriesLastForever;
+
+        public override void AI(Projectile projectile) {
+            // Make sentries last forever 
+            projectile.timeLeft = 2;
+
+            // Make them die if their owner is too far away
+            var owner = Main.player[projectile.owner];
+            float maxDist = 500f * 16f * (float)ServerConfig.Instance.SentriesDespawnDistanceMultiplier;
+            if (projectile.Center.DistanceSQ(owner.Center) > maxDist * maxDist) {
+                projectile.Kill();
+            }
+        }
+    }
+}
