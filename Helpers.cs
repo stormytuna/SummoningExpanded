e7be4cb811ce2836827f006bea751e9e00cd43f4
stormytuna <stormytuna@outlook.com>
2022-11-15 16:38:39 +0000

    Add helpers class

---
+++ Helpers.cs
@@ -0,0 +1,11 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace SummoningExpanded {
+    public static class Helpers {
+        /// <summary>Returns whether or not the item is a Minion or Sentry weapon, but not a Whip weapon</summary>
+        public static bool MinionOrSentryWeapon(this Item item) {
+            return item.CountsAsClass(DamageClass.Summon) && !item.CountsAsClass(DamageClass.SummonMeleeSpeed);
+        }
+    }
+}
