14ec926c90e61ce0a5437511367dfd85d424f5ea
stormytuna <stormytuna@outlook.com>
2022-11-18 13:21:14 +0000

    Add rope whip

---
+++ Content/Items/Whips/RopeWhip.cs
@@ -0,0 +1,29 @@
+﻿using SummoningExpanded.Content.Projectiles.Whips;
+using Terraria.GameContent.Creative;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace SummoningExpanded.Content.Items.Whips {
+    public class RopeWhip : ModItem {
+        public override void SetStaticDefaults() {
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+        }
+
+        public override void SetDefaults() {
+            Item.DefaultToWhip(ModContent.ProjectileType<RopeWhipProjectile>(), 10, 1f, 6f, 35);
+
+            Item.shootSpeed = 4;
+            Item.rare = ItemRarityID.Blue;
+            Item.channel = true;
+        }
+
+        public override void AddRecipes() {
+            CreateRecipe()
+                .AddIngredient(ItemID.Rope, 30)
+                .AddTile(TileID.WorkBenches)
+                .Register();
+        }
+
+        public override bool MeleePrefix() => true;
+    }
+}
---
+++ Content/Projectiles/Whips/RopeWhipProjectile.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.GameContent;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace SummoningExpanded.Content.Projectiles.Whips {
+    public class RopeWhipProjectile : ModProjectile {
+        public override void SetStaticDefaults() {
+            DisplayName.SetDefault("Rope Whip");
+            ProjectileID.Sets.IsAWhip[Type] = true;
+        }
+
+        public override void SetDefaults() {
+            Projectile.DefaultToWhip();
+
+            Projectile.WhipSettings.Segments = 10;
+            Projectile.WhipSettings.RangeMultiplier = 0.5f;
+        }
+
+        private void DrawLine(List<Vector2> points) {
+            Texture2D texture = TextureAssets.FishingLine.Value;
+            Rectangle frame = texture.Frame();
+            Vector2 origin = new Vector2(frame.Width / 2, 2);
+
+            Vector2 pos = points[0];
+            for (int i = 0; i < points.Count - 1; i++) {
+                Vector2 element = points[i];
+                Vector2 diff = points[i + 1] - element;
+
+                float rotation = diff.ToRotation() - MathHelper.PiOver2;
+                Color color = Lighting.GetColor(element.ToTileCoordinates(), Color.White);
+                Vector2 scale = new Vector2(1, (diff.Length() + 2) / frame.Height);
+
+                Main.EntitySpriteDraw(texture, pos - Main.screenPosition, frame, color, rotation, origin, scale, SpriteEffects.None, 0);
+
+                pos += diff;
+            }
+        }
+
+        public override bool PreDraw(ref Color lightColor) {
+            List<Vector2> points = new();
+            Projectile.FillWhipControlPoints(Projectile, points);
+
+            DrawLine(points);
+
+            Main.DrawWhip_WhipBland(Projectile, points);
+
+            return false;
+        }
+    }
+}
